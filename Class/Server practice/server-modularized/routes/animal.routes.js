6a7c2766071cef403279d4460c54a8c2bf8d8206
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-03-09 12:16:38 -0500

    added practice

--- Class/Server practice/server-modularized/routes/animal.routes.js
+++ Class/Server practice/server-modularized/routes/animal.routes.js
@@ -2,5 +2,10 @@ const AnimalController = require('../controllers/animal.controller')
 
 
 module.exports = (app) => {
+    app.get('/api/animals', AnimalController.findAllAnimals)
     app.post('/api/animals', AnimalController.createNewAnimal)
+    app.get('/api/animals/:id', AnimalController.findOneAnimal)
+    app.put('/api/animals/:id', AnimalController.updateAnimal)
+    app.delete('/api/animals/:id', AnimalController.deleteAnimal)
+
 }
