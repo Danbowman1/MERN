bfcca17e47f0b24fc35ebdd4050f697a3cc8c78a
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-03-09 19:29:27 -0500

    switched to async await and added photo

--- React/luke-apiwalker/src/components/Display.js
+++ React/luke-apiwalker/src/components/Display.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "./Form";
 import { useParams, Link } from "react-router-dom";
+import obi from '../assets/abiwan.jpg'
 
 
 
@@ -9,7 +10,6 @@ const Display = (props) => {
 
     const {type, id} = useParams()
     const [person, setPerson] = useState({})
-    // const [planets, setPlanets] = useState({})
     const [category, setCategory] = useState('')
     const [categoryKeys, setCategoryKeys] = useState([])
     const [error, setError] = useState(false)
@@ -17,42 +17,64 @@ const Display = (props) => {
     const [displayWorld, setDisplayWorld] = useState({})
 
     useEffect(()=> {
-        axios.get(`https://swapi.dev/api/${type}/${id}`)
-            .then((res)=>{
-                setError(false)
-                if(type === 'people'){
-                    console.log(res.data)
-                    setPerson(res.data)
-                    setHomeWorld(res.data.homeworld)
-                    setCategory(null)
-                } else {
-                    console.log(res.data)
-                    setCategory(res.data)
-                    setCategoryKeys(Object.keys(res.data))
+
+        const getType = async () => {
+
+                try {
+                    const res = await axios.get(`https://swapi.dev/api/${type}/${id}`)
+            console.log(res)
+            console.log(res.data)
+            setError(false)
+
+            if(type === 'people'){
+                console.log(res.data)
+                setPerson(res.data)
+                setCategory(null)
+                const homeWorld = res.data.homeworld
+                const res2 = await axios.get(homeWorld)
+                setDisplayWorld({
+                    name: res2.data.name,
+                    url: res2.data.url.match(/\d+/g),
+                })
+
+            } else {
+                console.log(res.data)
+                setCategory(res.data)
+                setCategoryKeys(Object.keys(res.data))
+            }
+                    
+                } catch (err) {
+                    console.log(err)
+                    setError(true)
                 }
-            })
-            .catch((err)=>{
-                setError(true)
-                console.log(err)
-            })
+
+        }
+
+        
+            
+                
+                
+            
+        getType()
+
     }, [type, id])
 
-    useEffect(()=> {
-        if(homeWorld){
-            axios.get(homeWorld)
-                .then((res)=>{
-                    console.log(res.data)
-                    setDisplayWorld({
-                        name: res.data.name,
-                        url: res.data.url.match(/\d+/g),
-                    })
-                })
-                .catch((err)=>console.log(err))
-        }
-        else{
-            console.log('No home world')
-        }
-    }, [homeWorld])
+    // useEffect(()=> {
+    //     if(homeWorld){
+    //         axios.get(homeWorld)
+    //             .then((res)=>{
+    //                 console.log(res.data)
+    //                 setDisplayWorld({
+    //                     name: res.data.name,
+    //                     url: res.data.url.match(/\d+/g),
+    //                 })
+    //             })
+    //             .catch((err)=>console.log(err))
+    //     }
+    //     else{
+    //         console.log('No home world')
+    //     }
+    // }, [homeWorld])
 
     return (
         <div>  
@@ -61,7 +83,7 @@ const Display = (props) => {
                 error?
                 <div>
                 <p>These are not the droids you are looking for!</p>
-            
+                <img src={obi} alt="obi"/>
                 </div>
                 :null
             }
