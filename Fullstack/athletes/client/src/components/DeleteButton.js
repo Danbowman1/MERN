13b7b436709a3019c00bffea90c8ff69acc6f1b2
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-03-27 11:08:30 -0400

    Finished Demo

--- Fullstack/athletes/client/src/components/DeleteButton.js
+++ Fullstack/athletes/client/src/components/DeleteButton.js
@@ -1,9 +1,24 @@
+import axios from 'axios'
 import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
 const DeleteButton = () => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const onClickHandler = (e) =>{
+        axios.delete(`http://localhost:8000/api/athletes/${id}`)
+            .then(res=>{
+                console.log(res)
+                navigate("/")
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
     return (
         <div>
-        <button>Delete</button>
+        <button onChange={onClickHandler}>Delete</button>
         </div>
     )
 }
